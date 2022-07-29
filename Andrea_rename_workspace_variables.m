693f44eec29fcba9354088ebe693cb068dc9d675
Ashu156 <35688705+Ashu156@users.noreply.github.com>
2022-07-29 19:34:13 +0200

    Add files via upload

---
+++ Andrea_rename_workspace_variables.m
@@ -0,0 +1,61 @@
+%%
+clear;
+close all;
+clc;
+
+%% Loading the raw data 
+
+[file, path] = uigetfile; % choose the file using GUI
+load(strcat(path, file)); % load the file
+
+%% 
+s1 = whos;               % Extract all variables from the workspace
+s2 = {s1.name};          % Extract the name of workspace variables
+
+%% Renaming the old varibales such that they have the rat ID
+
+ID = path(end-10:end-1);
+ID_new = ID;
+ID_new(ID == ' ') = '_'; 
+animal_ID = strcat('_', ID_new); % rat ID
+
+% Old varibales to be renamed
+
+oldname = s2;
+oldname(strncmpi(oldname,'path', length(oldname))) = [];
+oldname(strncmpi(oldname,'file', length(oldname))) = [];
+% oldname = {'lfp'; 'tx'};
+
+% Initializing the cell array for storing renamed variables
+newname = cell(size(oldname));
+
+% Renaming the old variables
+for i = 1:length(oldname)
+    
+    % Check if the old variables exist in the workspace
+    teststr = ['exist(''', oldname{1, i}, ''', ''var'')'];
+    result = evalin('base', teststr); 
+    
+    % If the old variables don't exist, throw an error
+    if result ~= 1
+        error(['A variable named ''', oldname{1, i}, ''' does not exist in the base workspace'])
+    end
+    
+    % New variable names
+    newname{1, i} = strcat(oldname{1, i}, animal_ID);
+    
+    % Assigning the old values to the renamed variables
+    str = [newname{1, i}, ' = ', oldname{1, i}, ';'];
+    
+    try
+        evalin('base',str)
+    catch
+        error('The rename failed')
+    end
+end
+
+%% Saving the renamed variables
+
+save(strcat(path, file(1:end-4)), newname{:}); % save the results
+
+%% end of script
